99318699bf4bc101171e89a06549b61c0ca40524
Tomas Jira <tom.jira@gmail.com>
2024-04-03 16:15:08 +0200

    add some random examples and improve the matrix plotting script

--- script/plotmat.py
+++ script/plotmat.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     parser.add_argument("-h", "--help", action="help", default=ap.SUPPRESS, help="Show this help message and exit.")
     parser.add_argument("-d", "--dimension", type=int, default=1, help="Potential dimension.")
     parser.add_argument("-n", "--normalize", action="store_true", help="Normalize provided data.")
+    parser.add_argument("-c", "--complex", action="store_true", help="Treat the data as complex numbers and plot their absolute value.")
     parser.add_argument("--colors", nargs="+", help="Colors for the data.")
     parser.add_argument("--legend", nargs="+", help="Legend for the data.")
     parser.add_argument("--xlim", nargs=2, type=float, help="Limits on the horizontal axis.")
@@ -36,14 +37,14 @@ if __name__ == "__main__":
     # add colors if provided
     if args.colors: plt.gca().set_prop_cycle(cl.cycler(color=args.colors))
 
-    # plot the matrices
-    if args.dimension == 2:
-        for M in mats:
-            plt.axes(projection="3d").plot_trisurf(M[:, 0], M[:, 1], M[:, 2])
-    else:
-        for M in mats:
-            for C in M.T[1:]:
-                plt.plot(M[:, 0], C)
+    # convert matrices to complex if required
+    if args.complex: mats = [np.hstack((mat[:, 0].reshape(-1, 1), mat[:, 1::2] + 1j * mat[:, 2::2])) for mat in mats]
+
+    # plot the matrices in 1D
+    [[plt.plot(np.real(M[:, 0]), np.abs(C) if args.complex else C) for C in M[:, 1:].T] for M in mats if args.dimension == 1]
+
+    # plot the matrices in 2D
+    [plt.axes(projection="3d").plot_trisurf(M[:, 0], M[:, 1], M[:, 2]) for M in mats if args.dimension == 2]
 
     # add legend, labels and limits if provided
     if args.legend: plt.legend(args.legend)
