c5d7a7ec872be7008b5f8d256f3e07bd25c33c1d
Tomas Jira <tom.jira@gmail.com>
2024-05-22 08:31:13 +0200

    fix the NAD1D test

--- test/nad1d.cpp
+++ test/nad1d.cpp
@@ -7,8 +7,9 @@ int test_nad1d(int, char**) {
     ModelSystem system(2000, {{"0.01*tanh(0.6*x)", "0.001*exp(-x^2)"}, {"0.001*exp(-x^2)", "-0.01*tanh(0.6*x)"}}, {"x"}, {-24, 24}, 4096);
 
     // create the classical solver options with some parameters
-    ModelSolver::OptionsDynamics opt; opt.iters = 350, opt.momentum = {10.95}, opt.position = {-10}, opt.state = 1;
-    opt.seed = 1, opt.step = 10, opt.trajs = 10, opt.gradient = {"0.006/cosh(0.6*x)^2", "-0.006/cosh(0.6*x)^2"};
+    ModelSolver::OptionsDynamics opt; opt.iters = 350, opt.momentum = {10.95}, opt.position = {-10}, opt.state = 1; opt.seed = 1, opt.step = 10;
+    opt.trajs = 10, opt.gradient = {{"0.006/cosh(0.6*x)^2", "-0.002*x*exp(-x^2)"}, {"-0.002*x*exp(-x^2)", "-0.006/cosh(0.6*x)^2"}};
+    opt.adiabatic = false, opt.savetraj = false;
 
     // create the solver and perform the dynamics
     Result res = ModelSolver(opt).run(system, {}, false);
