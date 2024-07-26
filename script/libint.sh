599733f33474cc45cd4409cb3e71fa82f4532971
Tomas Jira <tom.jira@gmail.com>
2024-07-26 18:41:31 +0200

    update libint max angular momentum

--- script/libint.sh
+++ script/libint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # clone the repository
-git clone https://github.com/evaleev/libint.git libint
+git clone --depth 1 https://github.com/evaleev/libint.git libint
 
 # compile libint
-cd libint && ./autogen.sh && ./configure CXX=g++ CXXFLAGS="-march=native -s -O3" --prefix="$PWD/install" --enable-1body=1 --enable-eri=1 --with-max-am=6 && make -j2 && make install && cd ..
+cd libint && ./autogen.sh && ./configure CXX=g++ CXXFLAGS="-march=native -s -O3" --prefix="$PWD/install" --enable-1body=1 --enable-eri=1 --with-max-am=4 && make -j2 && make install && cd ..
