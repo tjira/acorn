e9bf5285ba16f884f128835cd315cbdf163c6707
Tomas Jira <tom.jira@gmail.com>
2024-07-27 09:33:18 +0200

    add torch to external dependencies

--- script/libtorch.sh
+++ script/libtorch.sh
@@ -4,9 +4,7 @@
 wget https://github.com/pytorch/pytorch/releases/download/v2.4.0/pytorch-v2.4.0.tar.gz && tar -xzvf pytorch-v2.4.0.tar.gz && mv pytorch-v2.4.0 libtorch && rm pytorch-v2.4.0.tar.gz
 
 # configure libtorch
-cd libtorch && cmake -B build -DBUILD_PYTHON=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/install" -DUSE_FBGEMM=OFF && cd ..
+cd libtorch && cmake -B build -DBUILD_PYTHON=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/install" -DUSE_DISTRIBUTED=OFF -DUSE_FBGEMM=OFF -DUSE_OPENMP=OFF && cd ..
 
 # build and install libtorch
 cd libtorch && cmake --build build --parallel 2 && cmake --install build && cd ..
-
-# pip install pyyaml typing-extensions
