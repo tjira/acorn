976745563451f4854f0490c3ce1afd0a1cd751c6
Tomas Jira <tom.jira@gmail.com>
2024-10-20 20:42:32 +0200

    try to build the whole torch on github

--- script/library/libtorch.sh
+++ script/library/libtorch.sh
@@ -4,7 +4,7 @@
 mkdir -p external && wget -O external/libtorch.tar.gz https://github.com/pytorch/pytorch/releases/download/v2.5.0/pytorch-v2.5.0.tar.gz && cd external && rm -rf libtorch && tar -xzvf libtorch* && mv pytorch* libtorch && cd -
 
 # configure the library
-cd external/libtorch && cmake -B build -DBUILD_PYTHON=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/install" -DUSE_CUDA=OFF -DUSE_DISTRIBUTED=OFF -DUSE_FBGEMM=OFF -DUSE_MAGMA=OFF -DUSE_MKLDNN=OFF -DUSE_NNPACK=OFF -DUSE_NUMPY=OFF -DUSE_OPENMP=OFF -DUSE_PYTORCH_QNNPACK=OFF -DUSE_ROCM=OFF -DUSE_XNNPACK=OFF -DUSE_XPU=OFF && cd -
+cd external/libtorch && cmake -B build -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/install" -DUSE_CUDA=OFF && cd -
 
 # compile and install the library
 cd external/libtorch && cmake --build build --parallel 2 && cmake --install build && cd -
