b8dfa6e2968a20bccb917d670b1510a418ba8bf3
Tomas Jira <tom.jira@gmail.com>
2024-10-21 11:36:34 +0200

    update library scripts

--- script/library/libfftw.sh
+++ script/library/libfftw.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+# compile parameters
+SHARED="OFF"; CORES=2; if [ $# -ne 0 ] && [ $# -ne 2 ]; then echo "ARGUMENTS: SHARED(ON/OFF) CORES(1,2,...)"; exit 1; fi; if [ $# -eq 2 ]; then SHARED=$1; CORES=$2; fi
+
 # download the library
 mkdir -p external && wget -O external/libfftw.tar.gz https://www.fftw.org/fftw-3.3.10.tar.gz && cd external && rm -rf libfftw && tar -xzvf libfftw* && mv fftw* libfftw && cd -
 
 # configure the library
-cd external/libfftw && cmake -B build -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/install" && cd -
+cd external/libfftw && cmake -B build -DBUILD_SHARED_LIBS=$SHARED -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/install" && cd -
 
 # compile and install the library
-cd external/libfftw && cmake --build build --parallel 2 && cmake --install build && cd -
+cd external/libfftw && cmake --build build --parallel $CORES && cmake --install build && cd -
 
 # copy the compiled library
 cp -r external/libfftw/install/include external/libfftw/install/lib external/
-
-# rename the compiled library
-cd external/lib && mv libfftw3.a libfftw.a ; cd -
--- script/library/libtorch.sh
+++ script/library/libtorch.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# compile parameters
+SHARED="OFF"; CORES=2; if [ $# -ne 0 ] && [ $# -ne 2 ]; then echo "ARGUMENTS: SHARED(ON/OFF) CORES(1,2,...)"; exit 1; fi; if [ $# -eq 2 ]; then SHARED=$1; CORES=$2; fi
+
 # download the library
 mkdir -p external && wget -O external/libtorch.tar.gz https://github.com/pytorch/pytorch/releases/download/v2.5.0/pytorch-v2.5.0.tar.gz && cd external && rm -rf libtorch && tar -xzvf libtorch* && mv pytorch* libtorch && cd -
 
 # configure the library
-cd external/libtorch && cmake -B build -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/install" -DUSE_CUDA=OFF -DUSE_ROCM=OFF && cd -
+cd external/libtorch && cmake -B build -DBUILD_PYTHON=OFF -DBUILD_SHARED_LIBS=$SHARED -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PWD/install" -DUSE_CUDA=OFF -DUSE_DISTRIBUTED=OFF -DUSE_FBGEMM=OFF -DUSE_MKLDNN=OFF -DUSE_PYTORCH_QNNPACK=OFF -DUSE_ROCM=OFF -DUSE_XNNPACK=OFF -DUSE_XPU=OFF && cd -
 
 # compile and install the library
-cd external/libtorch && cmake --build build --parallel 2 && cmake --install build && cd -
+cd external/libtorch && cmake --build build --parallel $CORES && cmake --install build && cd -
 
 # copy the compiled library
 cp -r external/libtorch/install/include external/libtorch/install/lib external/
