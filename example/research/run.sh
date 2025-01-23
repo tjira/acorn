30850a93100e8ac038bc9d66f42da980311dee58
Tomas Jira <tom.jira@gmail.com>
2025-01-23 13:34:04 +0100

    edit example plotting to work on non-GUI sessions

--- example/research/run.sh
+++ example/research/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BIN=../../zig-out/x86_64-linux/example; PLOT=../../python/plot.py; export PLOT;
+BIN=../../zig-out/x86_64-linux/example; PLOT=../../python/plot.py; export PLOT; export MPLBACKEND=agg
 
 zig build --release=fast && rm -rf result *.mat *.png && mkdir -p result && for FILE in *.zig; do
     [[ "$#" -eq 1 ]] && [[ $1 != $FILE ]] && continue; $BIN/${FILE%.*} && [[ -f ./analyze/${FILE%.*}.sh ]] && ./analyze/${FILE%.*}.sh && mkdir result/${FILE%.*} &&  mv *.mat *.png result/${FILE%.*};
