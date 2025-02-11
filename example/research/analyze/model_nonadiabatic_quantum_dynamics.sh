76cbe5211bf3264170930e6c021c7373c73f5b20
Tomas Jira <tom.jira@gmail.com>
2025-02-11 09:43:06 +0100

    fix plotting for the nonadiabatic dynamics example

--- example/research/analyze/model_nonadiabatic_quantum_dynamics.sh
+++ example/research/analyze/model_nonadiabatic_quantum_dynamics.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-plot.py WAVEFUNCTION_DIABATIC_tully1D_1.mat:0,1,2,3~POTENTIAL_DIABATIC_tully1D_1.mat:0-0,1-0,2-3,3-3   --animate 4 --scale 0.01 --xlabel "Coordinate (a.u)" --ylabel "Value" --output WAVEFUNCTION_DIABATIC_tully1D_1  --gif
-plot.py WAVEFUNCTION_ADIABATIC_tully1D_1.mat:0,1,2,3~POTENTIAL_ADIABATIC_tully1D_1.mat:0-0,1-0,2-3,3-3 --animate 4 --scale 0.01 --xlabel "Coordinate (a.u)" --ylabel "Value" --output WAVEFUNCTION_ADIABATIC_tully1D_1 --gif
+$PLOT WAVEFUNCTION_DIABATIC_tully1D_1.mat:0,1,2,3~POTENTIAL_DIABATIC_tully1D_1.mat:0-0,1-0,2-3,3-3   --animate 4 --scale 0.01 --xlabel "Coordinate (a.u)" --ylabel "Value" --output WAVEFUNCTION_DIABATIC_tully1D_1  --gif
+$PLOT WAVEFUNCTION_ADIABATIC_tully1D_1.mat:0,1,2,3~POTENTIAL_ADIABATIC_tully1D_1.mat:0-0,1-0,2-3,3-3 --animate 4 --scale 0.01 --xlabel "Coordinate (a.u)" --ylabel "Value" --output WAVEFUNCTION_ADIABATIC_tully1D_1 --gif
 
-plot.py POPULATION_DIABATIC_tully1D_1.mat  --xlabel "Time (a.u)" --ylabel "Population" --legend "S0" "S1" --output POPULATION_DIABATIC_tully1D_1  --png
-plot.py POPULATION_ADIABATIC_tully1D_1.mat --xlabel "Time (a.u)" --ylabel "Population" --legend "S0" "S1" --output POPULATION_ADIABATIC_tully1D_1 --png
+$PLOT POPULATION_DIABATIC_tully1D_1.mat  --xlabel "Time (a.u)" --ylabel "Population" --legend "S0" "S1" --output POPULATION_DIABATIC_tully1D_1  --png
+$PLOT POPULATION_ADIABATIC_tully1D_1.mat --xlabel "Time (a.u)" --ylabel "Population" --legend "S0" "S1" --output POPULATION_ADIABATIC_tully1D_1 --png
 
 montage POPULATION_DIABATIC_tully1D_1.png POPULATION_ADIABATIC_tully1D_1.png -mode concatenate -tile x1 POPULATION_tully1D_1.png
 
