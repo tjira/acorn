d3d7812551f3656ec92331c91e859e123ca878c2
Tomas Jira <tom.jira@gmail.com>
2025-02-18 16:04:36 +0100

    improve the neqdet.py script

--- education/python/neqdet.py
+++ education/python/neqdet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import argparse as ap, matplotlib.pyplot as plt, numpy as np
+import argparse as ap, matplotlib.pyplot as plt, numpy as np, scipy as sp, scipy.linalg
 
 if __name__ == "__main__":
     # SECTION FOR PARSING COMMAND LINE ARGUMENTS =======================================================================================================================================================
@@ -43,7 +43,8 @@ if __name__ == "__main__":
     k = 2 * np.pi * np.fft.fftfreq(args.points, r[1] - r[0])
 
     # define the potential and extract the grid spacing
-    V = 0.5 * r ** 2; dr = r[1] - r[0]
+    # V = np.array([[0.5 * r ** 2]]).transpose(2, 0, 1); dr = r[1] - r[0]
+    V = np.array([[0.01 * np.tanh(0.6 * r), 0.001 * np.exp(-r**2)], [0.001 * np.exp(-r**2), -0.01 * np.tanh(0.6 * r)]]).transpose(2, 0, 1); dr = r[1] - r[0]
 
     # PERFORM THE PROPAGATIONS =========================================================================================================================================================================
 
@@ -54,39 +55,42 @@ if __name__ == "__main__":
         print("%sPROPAGATION OF STATE %d " % ("\n " if i else "", i))
 
         # create the initial wavefunction from the provided guess
-        psi = eval(args.guess.replace("exp", "np.exp").replace("r1", "r")).astype(np.complex128);
+        # psi = np.zeros((args.points, V.shape[1]), dtype=complex); psi[:, 0] = np.exp(-(r - 1)**2 + 0j * r)
+        psi = np.zeros((args.points, V.shape[1]), dtype=complex); psi[:, 1] = np.exp(-(r + 10)**2 + 10j * r)
 
         # calculate the propagators for each point in the grid
-        K = np.exp(-0.5 * (1j if args.real else 1) * args.timestep * k ** 2 / args.mass)
-        R = np.exp(-0.5 * (1j if args.real else 1) * args.timestep * V                 )
+        K = np.array([sp.linalg.expm(-0.5 * (1j if args.real else 1) * args.timestep * k[i] ** 2 / args.mass * np.eye(psi.shape[1])) for i in range(args.points)])
+        R = np.array([sp.linalg.expm(-0.5 * (1j if args.real else 1) * args.timestep                         * V[i]                ) for i in range(args.points)])
 
         # print the propagation header
-        print("%6s %12s %12s %12s" % ("ITER", "EKIN", "EPOT", "ETOT"))
+        print("%6s %12s %12s %12s%s" % ("ITER", "EKIN", "EPOT", "ETOT", "".join(" %12s" % ("RHO[%d, %d]" % (i, i)) for i in range(psi.shape[1]))))
 
         # propagate the wavefunction
         for j in range(args.iterations + 1):
 
             # propagate the wavefunction from second iteration
-            if (j): psi = R * np.fft.ifft(K * np.fft.fft(R * psi))
+            if (j): psi = np.einsum("ijk,ik->ij", R, np.fft.ifft(np.einsum("ijk,ik->ij", K, np.fft.fft(np.einsum("ijk,ik->ij", R, psi), axis=0)), axis=0))
 
             # orthogonalize the wavefunction
             for wfn in wfnopt: psi -= np.trapz(wfn.conj() * psi, dx=dr) * wfn
 
             # normalize the wavefunction
-            psi /= np.sqrt(np.sum(psi.conj() * psi) * dr)
+            if not args.real or j == 0: psi /= np.sqrt(np.sum(psi.conj() * psi) * dr)
 
-            # integrate the expressions needed for kinetic and potential energy
-            Ekin = np.trapz(psi.conj() * np.fft.ifft(k ** 2 * np.fft.fft(psi)), dx=dr).real
-            Epot = np.trapz(psi.conj() * V * psi,                               dx=dr).real
+            # calculate the kinetic energy
+            Ekin = np.sum(psi.conj() * np.fft.ifft(0.5 * k[:, None]**2 / args.mass * np.fft.fft(psi, axis=0), axis=0)).real * dr;
 
-            # calculate the total energy
-            Ekin = 0.5 * Ekin / args.mass; Etot = Ekin + Epot
+            # calculate the potential energy
+            Epot = np.einsum("ij,ijk,ik->", psi.conj(), V, psi).real * dr
+            
+            # calculate the density matrix
+            rho = psi.T @ psi.conj() * dr
 
             # print the iteration info
-            if (j % 100 == 0): print("%6d %12.6f %12.6f %12.6f" % (j, Ekin, Epot, Etot))
+            if j % 100 == 0: print("%6d %12.6f %12.6f %12.6f%s" % (j, Ekin, Epot, Ekin + Epot, "".join(" %12.6f" % (rho[i, i].real) for i in range(rho.shape[0]))))
 
         # append the optimized wavefunction to the container
-        wfnopt.append(psi.copy())
+        if not args.real: wfnopt.append(psi.copy())
 
     # plt.plot(r, psi.real)
     # plt.plot(r, psi.imag)
