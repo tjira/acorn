9a090a9d005f96591c204d46ea0bd3b6f1e3ce6a
Tomas Jira <tom.jira@gmail.com>
2025-03-06 19:12:25 +0100

    add the ability to offset and scale plots in lines.py

--- python/lines.py
+++ python/lines.py
@@ -27,7 +27,9 @@ parser.add_argument("--ylabel", type=str, help="The an y-axis label.")
 parser.add_argument("--ylim", type=float, nargs=2, help="The y-axis limits of the plot.")
 
 # additional file arguments
-parser.add_argument("-e", "--errors", nargs="+", help="Error bars for the plotted lines.")
+parser.add_argument("--errors", nargs="+", help="Error bars for the plotted lines.")
+parser.add_argument("--offsets", nargs="+", help="Vertical offsets for the plotted lines.")
+parser.add_argument("--scales", nargs="+", help="Scales for the plotted lines.")
 
 # add positional arguments and parse the arguments
 parser.add_argument("files", nargs="+", help="The data files to plot."); args = parser.parse_args()
@@ -35,14 +37,23 @@ parser.add_argument("files", nargs="+", help="The data files to plot."); args = 
 # function to load the data from the input string as well as the columns to plot
 load = lambda s: (d := np.loadtxt((p := s.split(":", 1))[0], ndmin=2, skiprows=1), np.array(p[1].split(","), int) if len(p) == 2 and p[1] else np.arange(d.shape[1] - 1))
 
+# function to return the iterator for the data and columns
+dcit = lambda d, c: ((d_i, j) for i, (d_i, c_i) in enumerate(zip(d, c)) for j in c_i)
+
 # load the data with plotted columns and get the total number of lines
-data, dcols = zip(*[load(file) for file in args.files]); nline = sum(len(dcols[i]) for i in range(len(dcols)))
+data, data_cols = zip(*[load(file) for file in args.files]); nline = sum(len(data_cols[i]) for i in range(len(data_cols)))
 
 # load the error bars if provided
-if args.errors: errors, ecols = zip(*[load(error) for error in args.errors[1:]])
+data_errors, cols_errors = zip(*[load(file) for file in args.errors[1:]]) if args.errors else ((), ())
 
 # get the line indices for the error bars
-if args.errors: elines = list(map(lambda x: int(x), args.errors[0].split(","))) if args.errors[0] != "all" else np.arange(sum([len(ecol) for ecol in ecols]))
+lind_errors  = (list(map(lambda x: int(x), args.errors [0].split(","))) if args.errors [0] != "all" else np.arange(sum([len(col) for col in cols_errors]))) if args.errors  else []
+lind_offsets = (list(map(lambda x: int(x), args.offsets[0].split(","))) if args.offsets[0] != "all" else np.arange(len(args.offsets) - 1                 )) if args.offsets else []
+lind_scales  = (list(map(lambda x: int(x), args.scales [0].split(","))) if args.scales [0] != "all" else np.arange(len(args.scales ) - 1                 )) if args.scales  else []
+
+# add the scales and offsets to the data
+for i, (data_i, j) in enumerate(dcit(data, data_cols)): data_i[:, j + 1] *= float(args.scales [lind_scales .index(i) + 1]) if i in lind_scales  else 1
+for i, (data_i, j) in enumerate(dcit(data, data_cols)): data_i[:, j + 1] += float(args.offsets[lind_offsets.index(i) + 1]) if i in lind_offsets else 0
 
 # create the figure and the container for plots and error bars
 fig, ax = plt.subplots(dpi=args.dpi, figsize=(args.figsize[1], args.figsize[0])); plots, ebars = [], []
@@ -51,23 +62,23 @@ fig, ax = plt.subplots(dpi=args.dpi, figsize=(args.figsize[1], args.figsize[0]))
 cmap = plt.get_cmap(args.colormap)(np.linspace(0.1, 0.9, nline) if (ncolor := plt.get_cmap(args.colormap).N) > 20 else np.linspace(0, 1, ncolor))
 
 # loop over data and its columns to plot
-for datai, j in ((datai, j) for i, (datai, dcolsi) in enumerate(zip(data, dcols)) for j in dcolsi):
+for data_i, j in dcit(data, data_cols):
 
     # plot the data and append the plot to the list
-    plots.append(ax.plot(datai[:, 0], datai[:, j + 1], color=cmap[len(plots) % len(cmap)]))
+    plots.append(ax.plot(data_i[:, 0], data_i[:, j + 1], color=cmap[len(plots) % len(cmap)]))
 
 # loop over the error bars and their columns
-for (errorsi, j) in ((errorsi, j) for i, (errorsi, ecolsi) in enumerate(zip(errors if args.errors else [], ecols if args.errors else [])) for j in ecolsi):
+for (data_errors_i, j) in dcit(data_errors, cols_errors):
 
     # get the top and bottom lines
-    top = plots[elines[len(ebars)]][0].get_ydata() + errorsi[:, j + 1]
-    bot = plots[elines[len(ebars)]][0].get_ydata() - errorsi[:, j + 1]
+    top = plots[lind_errors[len(ebars)]][0].get_ydata() + data_errors_i[:, j + 1]
+    bot = plots[lind_errors[len(ebars)]][0].get_ydata() - data_errors_i[:, j + 1]
 
     # fill the area between the top and bottom line and append the plot to the list
-    ebars.append(ax.fill_between(errorsi[:, 0], bot, top, color=cmap[elines[len(ebars)] % len(cmap)], alpha=0.2))
+    ebars.append(ax.fill_between(data_errors_i[:, 0], bot, top, color=cmap[lind_errors[len(ebars)] % len(cmap)], alpha=0.2))
 
 # set the default axis limits
-ax.set_xlim(min([datai[:, 0].min() for datai in data]), max([datai[:, 0].max() for datai in data]))
+ax.set_xlim(min([data_i[:, 0].min() for data_i in data]), max([data_i[:, 0].max() for data_i in data]))
 
 # add the legend
 if args.legend: ax.legend(args.legend, frameon=False)
