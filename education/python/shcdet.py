fix the HST TDC calculation according to my notes

education/python/shcdet.py
@@ -52,7 +52,7 @@ s = np.zeros((args.trajectories, args.iterations + 1), dtype=int) + args.state; 
 def fssh(i, substeps=10):
 
     # calculate the eigenvector overlap snd TDC
-    S = Us[-2].swapaxes(1, 2) @ Us[-1]; TDC = (S - np.transpose(S, (0, 2, 1))) / (2 * args.timestep)
+    S = Us[-1].swapaxes(1, 2) @ Us[-2]; TDC = (np.transpose(S, (0, 2, 1)) - S) / (2 * args.timestep)
 
     # define the time derivative of the electronic coefficients
     dC = lambda C: -1j * np.diag(Vs[-1][j]) * C - TDC[j] @ C