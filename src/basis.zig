b8da2896088c639ec3f2347ecd4f34d9e4085922
Tomas Jira <tom.jira@gmail.com>
2025-04-06 09:25:25 +0200

    small bug fix in basis file reading

--- src/basis.zig
+++ src/basis.zig
@@ -11,12 +11,14 @@ pub fn Basis(comptime T: type) type {
 
         /// Get the basis set for the given system and name.
         pub fn get(system: System(T), name: []const u8, allocator: std.mem.Allocator) !std.ArrayList(ContractedGaussian(T)) {
-            var basis = std.ArrayList(ContractedGaussian(T)).init(allocator); var parsed: std.json.Parsed(std.json.Value) = undefined; defer parsed.deinit();
+            var basis = std.ArrayList(ContractedGaussian(T)).init(allocator); var parsed: std.json.Parsed(std.json.Value) = undefined;
 
             const lower = try allocator.alloc(u8, name.len); defer allocator.free(lower); _ = std.ascii.lowerString(lower, name);
 
-            if      (std.mem.eql(u8, lower, "sto-3g")) {parsed = try std.json.parseFromSlice(std.json.Value, allocator, @embedFile("basis/sto-3g.json"), .{});}
-            else if (std.mem.eql(u8, lower, "6-31g" )) {parsed = try std.json.parseFromSlice(std.json.Value, allocator, @embedFile("basis/6-31g.json" ), .{});}
+            if      (std.mem.eql(u8, lower, "sto-3g"  )) {parsed = try std.json.parseFromSlice(std.json.Value, allocator, @embedFile("basis/sto-3g.json" ), .{});}
+            else if (std.mem.eql(u8, lower, "6-31g"   )) {parsed = try std.json.parseFromSlice(std.json.Value, allocator, @embedFile("basis/6-31g.json"  ), .{});}
+            else if (std.mem.eql(u8, lower, "cc-pvdz" )) {parsed = try std.json.parseFromSlice(std.json.Value, allocator, @embedFile("basis/cc-pvdz.json"), .{});}
+            else if (std.mem.eql(u8, lower, "cc-pvtz" )) {parsed = try std.json.parseFromSlice(std.json.Value, allocator, @embedFile("basis/cc-pvtz.json"), .{});}
 
             else return error.BasisNameNotFound;
 
@@ -30,27 +32,33 @@ pub fn Basis(comptime T: type) type {
 
                 for (shells) |shell| {
 
-                    const am        = shell.object.get("angular_momentum").?.array.items[0].integer    ;
-                    const exponents = shell.object.get("exponents"       ).?.array.items               ;
-                    const coefs     = shell.object.get("coefficients"    ).?.array.items[0].array.items;
+                    const am        = shell.object.get("angular_momentum").?.array.items[0].integer;
+                    const exponents = shell.object.get("exponents"       ).?.array.items           ;
+                    const coefs     = shell.object.get("coefficients"    ).?.array.items           ;
 
                     var a     = try allocator.alloc(T, 3            ); defer     allocator.free(a);
-                    var c     = try allocator.alloc(T, coefs.len    ); defer     allocator.free(c);
+                    var c     = try allocator.alloc(T, exponents.len); defer     allocator.free(c);
                     var alpha = try allocator.alloc(T, exponents.len); defer allocator.free(alpha);
 
-                    for (0..@as(usize, @intCast(2 * am + 1))) |j| {
+                    for (coefs) |coef| {
 
-                        a[0] = 0; a[1] = 0; a[2] = 0; if (am == 1) {a[j] = @as(T, @floatFromInt(am));} else if (am > 1) return error.InvalidAngularMomentum;
+                        for (0..alpha.len) |k| c[k] = try std.fmt.parseFloat(T, coef.array.items[k].string);
 
-                        for (0..c.len    ) |k| c[k]     = try std.fmt.parseFloat(T,     coefs[k].string);
-                        for (0..alpha.len) |k| alpha[k] = try std.fmt.parseFloat(T, exponents[k].string);
+                        for (0..@as(usize, @intCast(am + 1))) |lx| {
+                            for (0..@as(usize, @intCast(am + 1)) - lx) |ly| {
 
-                        try basis.append(try ContractedGaussian(T).init(system.getCoords(i), .{a[0], a[1], a[2]}, c, alpha, allocator));
+                                a[0] = @as(T, @floatFromInt(lx)); a[1] = @as(T, @floatFromInt(ly)); a[2] = @as(T, @floatFromInt(am)) - @as(T, @floatFromInt(lx + ly));
+
+                                for (0..alpha.len) |k| alpha[k] = try std.fmt.parseFloat(T, exponents[k].string);
+
+                                try basis.append(try ContractedGaussian(T).init(system.getCoords(i), .{a[0], a[1], a[2]}, c, alpha, allocator));
+                            }
+                        }
                     }
                 }
             }
 
-            return basis;
+            parsed.deinit(); return basis;
         }
     };
 }
