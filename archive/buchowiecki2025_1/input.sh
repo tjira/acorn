187b487a62103c1af37a3da728e1b9b1509c8ae3
Tomas Jira <tom.jira@gmail.com>
2025-07-09 16:25:59 +0200

    add some LZ comparison to the added 1D collision simulation

--- archive/buchowiecki2025_1/input.sh
+++ archive/buchowiecki2025_1/input.sh
@@ -4,12 +4,12 @@ cat << EOF > input_1D.json
 {
     "quantum_dynamics" : {
         "adiabatic" : true,
-        "iterations" : 4000,
+        "iterations" : 1,
         "mode" : [0, 1],
         "time_step" : 1,
         "grid" : {
-            "limits" : [0.001, 20],
-            "points" : 2048
+            "limits" : [0.001, 32],
+            "points" : 2
         },
         "hamiltonian" : {
             "dims" : 1,
@@ -36,18 +36,72 @@ EOF
 
 MASS=(1713.8065339025395 3209.8064174768374 4523.60441242962); ATOM=("H" "D" "T")
 
-for I in 0.11 $(seq 0.2 0.1 10); do
+for I in 0.035 $(seq 0.04 0.035 0.11); do
     for J in "${!MASS[@]}"; do
 
         M=${MASS[$J]}; A=${ATOM[$J]}; cp input_1D.json "input_1D_${A}_E=$I.json"
 
         sed -i 's/"mass" : 1/"mass" : '"$M"'/' "input_1D_${A}_E=$I.json"
 
+        sed -i 's/"iterations" : 1/"iterations" : 7000/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"time_step" : 1/"time_step" : 1/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"points" : 2/"points" : 4096/' "input_1D_${A}_E=$I.json"
+
         sed -i 's/"momentum" : \[0\]/"momentum" : \[-'"$(echo "sqrt(2*$M*$I)" | bc -l)"'\]/' "input_1D_${A}_E=$I.json"
 
         sed -i 's/"population" : "POPULATION.mat"/"population" : "POPULATION_'"$A"'_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
 
         sed -i 's/"position" : "POSITION.mat"/"position" : "POSITION_'"$A"'_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"density" : "DENSITY.mat"/"density" : "DENSITY_'"$A"'_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+    done
+done
+
+for I in 0.2 $(seq 0.5 1.5 11); do
+    for J in "${!MASS[@]}"; do
+
+        M=${MASS[$J]}; A=${ATOM[$J]}; cp input_1D.json "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"mass" : 1/"mass" : '"$M"'/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"iterations" : 1/"iterations" : 4000/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"time_step" : 1/"time_step" : 1/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"points" : 2/"points" : 4096/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"momentum" : \[0\]/"momentum" : \[-'"$(echo "sqrt(2*$M*$I)" | bc -l)"'\]/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"population" : "POPULATION.mat"/"population" : "POPULATION_'"$A"'_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"position" : "POSITION.mat"/"position" : "POSITION_'"$A"'_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"density" : "DENSITY.mat"/"density" : "DENSITY_'"$A"'_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+    done
+done
+
+for I in 15 $(seq 20 10 100); do
+    for J in "${!MASS[@]}"; do
+
+        M=${MASS[$J]}; A=${ATOM[$J]}; cp input_1D.json "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"mass" : 1/"mass" : '"$M"'/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"iterations" : 1/"iterations" : 4000/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"time_step" : 1/"time_step" : 0.1/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"points" : 2/"points" : 16384/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"momentum" : \[0\]/"momentum" : \[-'"$(echo "sqrt(2*$M*$I)" | bc -l)"'\]/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"population" : "POPULATION.mat"/"population" : "POPULATION_'"$A"'_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"position" : "POSITION.mat"/"position" : "POSITION_'"$A"'_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"density" : "DENSITY.mat"/"density" : "DENSITY_'"$A"'_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
     done
 done
 
--- archive/buchowiecki2025_1/plot.sh
+++ archive/buchowiecki2025_1/plot.sh
@@ -2,7 +2,7 @@
 
 lines.py POTENTIAL_DIA.mat:0,1,3 POTENTIAL_ADIA.mat:0,3 --figsize 3 12 --subplots 131 132 131 133 133 --title "Diabatic Potential" "SOC" "Adiabatic Potential" --xlabel "Coordinate (a.u.)" "Coordinate (a.u.)" "Coordinate (a.u.)" --xlim 1 10 0.001 15 1 10 --ylabel "Energy (a.u.)" "SOC (a.u.)" "Energy (a.u.)" --ylim -0.2 0.2 nan nan -0.2 0.2 --output POTENTIAL.png
 
-echo $(ls -l POPULATION_H_E=*.mat | wc -l)" 4" > PROBABILITY.mat
+echo $(ls -l POPULATION_H_E=*.mat | wc -l)" 4" > PROBABILITY_EXACT.mat
 
 for POP_H in $(ls POPULATION_H_E=*.mat); do
 
@@ -19,9 +19,13 @@ for POP_H in $(ls POPULATION_H_E=*.mat); do
     PROB_D=$(cat $POP_D | awk -v TIME=$TIME_D '$1 == TIME { print $3; exit }')
     PROB_T=$(cat $POP_T | awk -v TIME=$TIME_T '$1 == TIME { print $3; exit }')
 
-    echo "$E $PROB_H $PROB_D $PROB_T" >> PROBABILITY.mat
+    echo "$E $PROB_H $PROB_D $PROB_T" >> PROBABILITY_EXACT.mat
 done
 
-matsort -i PROBABILITY.mat -o PROBABILITY.mat
+matsort -i PROBABILITY_EXACT.mat -o PROBABILITY_EXACT.mat
 
-lines.py P.dat PROBABILITY.mat --alphas 0,1,2 0.7 0.7 0.7 --colors 3,4,5 "darkblue" "darkorange" "darkgreen" --legends every "Hydrogen (ref)" "Deuterium (ref)" "Tritium (ref)" "Hydrogen (my result)" "Deuterium (my result)" "Tritium (my result)" --legpos 0.05 1 --styles 3,4,5 dashed dashed dashed --xlabel "Kinetic Energy (a.u.)" --xlim 0.11 10 --ylabel "Charge Transfer Probability" --ylim 0 0.008 --output PROBABILITY.png
+lines.py P.dat PROBABILITY_EXACT.mat --alphas 0,1,2 0.7 0.7 0.7 --colors 3,4,5 "darkblue" "darkorange" "darkgreen" --legends every "Hydrogen (ref WP)" "Deuterium (ref WP)" "Tritium (ref WP)" "Hydrogen (my result WP)" "Deuterium (my result WP)" "Tritium (my result WP)" --legpos 0.05 1 --styles 3,4,5 dashed dashed dashed --xlabel "Kinetic Energy (a.u.)" --xlim 0.11 10 --ylabel "Charge Transfer Probability" --ylim 0 0.008 --output PROBABILITY_EXACT_COMPARE.png
+
+lines.py P_LZ.dat PROBABILITY_EXACT.mat P_LZ.dat PROBABILITY_EXACT.mat P_LZ.dat PROBABILITY_EXACT.mat --alphas 0,1,2,6,7,8,12,13,14 0.7 0.7 0.7 0.7 0.7 0.7 0.7 0.7 0.7 --colors 3,4,5,9,10,11,15,16,17 "darkblue" "darkorange" "darkgreen" "darkblue" "darkorange" "darkgreen" "darkblue" "darkorange" "darkgreen" --figsize 3 12 --legends every "Hydrogen (ref LZ)" "Deuterium (ref LZ)" "Tritium (ref LZ)" "Hydrogen (my result WP)" "Deuterium (my result WP)" "Tritium (my result WP)" "Hydrogen (ref LZ)" "Deuterium (ref LZ)" "Tritium (ref LZ)" "Hydrogen (my result WP)" "Deuterium (my result WP)" "Tritium (my result WP)" "Hydrogen (ref LZ)" "Deuterium (ref LZ)" "Tritium (ref LZ)" "Hydrogen (my result WP)" "Deuterium (my result WP)" "Tritium (my result WP)" --legpos 0.25 1 0.25 1 0.25 1 --styles 3,4,5,9,10,11,15,16,17 dashed dashed dashed dashed dashed dashed dashed dashed dashed --subplots 131 131 131 131 131 131 132 132 132 132 132 132 133 133 133 133 133 133 --xlabel "Kinetic Energy (a.u.)" "Kinetic Energy (a.u.)" "Kinetic Energy (a.u.)" --xlim 0.0347289 0.11 0.11 10 10 100 --ylabel "Charge Transfer Probability" "Charge Transfer Probability" "Charge Transfer Probability" --ylim 0 0.035 0 0.008 0 0.0012 --output PROBABILITY_SPLIT.png
+
+lines.py P_LZ.dat PROBABILITY_EXACT.mat --alphas 0,1,2 0.7 0.7 0.7 --colors 3,4,5 "darkblue" "darkorange" "darkgreen" --legends every "Hydrogen (ref WP)" "Deuterium (ref WP)" "Tritium (ref WP)" "Hydrogen (my result WP)" "Deuterium (my result WP)" "Tritium (my result WP)" --legpos 0.3 1 --styles 3,4,5 dashed dashed dashed --xlabel "Kinetic Energy (a.u.)" --xlim 0.0347289 100 --ylabel "Charge Transfer Probability" --ylim 0 0.001 --output PROBABILITY_FULL.png
