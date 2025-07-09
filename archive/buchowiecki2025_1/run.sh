9ab1af4119d4d3c71c510bd67e7c57149679c2bd
Tomas Jira <tom.jira@gmail.com>
2025-07-09 19:06:01 +0200

    fix a bug in run.sh in archive

--- archive/buchowiecki2025_1/run.sh
+++ archive/buchowiecki2025_1/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export OMP_NUM_THREADS=1; rm *=* && ./input.sh && PATH="$PWD:$PATH" acorn *.json && ./plot.sh
+export OMP_NUM_THREADS=1; rm -f *=* && ./input.sh && PATH="$PWD:$PATH" acorn *.json && ./plot.sh
