8cd4f915d7a481d2ae7a88a4429368f256defb3a
Tomas Jira <tom.jira@gmail.com>
2025-07-15 13:55:50 +0200

    edit the N-H energy grid

--- archive/buchowiecki2025_1/input.sh
+++ archive/buchowiecki2025_1/input.sh
@@ -94,7 +94,7 @@ EOF
 
 MASS=(1713.8065339025395 3209.8064174768374 4523.60441242962); ATOM=("H" "D" "T")
 
-for I in $(seq 0.035 0.005 1); do
+for I in $(seq 0.035 0.001 0.1); do
     for J in "${!MASS[@]}"; do
 
         M=${MASS[$J]}; A=${ATOM[$J]}; cp input_1D.json "input_1D_${A}_E=$I.json"
@@ -117,7 +117,30 @@ for I in $(seq 0.035 0.005 1); do
     done
 done
 
-for I in $(seq 1.1 0.1 10); do
+for I in $(seq 0.11 0.01 1); do
+    for J in "${!MASS[@]}"; do
+
+        M=${MASS[$J]}; A=${ATOM[$J]}; cp input_1D.json "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"mass" : 1/"mass" : '"$M"'/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"iterations" : 1/"iterations" : 7000/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"time_step" : 1/"time_step" : 1/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"points" : 2/"points" : 4096/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"momentum" : \[0\]/"momentum" : \[-'"$(echo "sqrt(2*$M*$I)" | bc -l)"'\]/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"population" : "POPULATION.mat"/"population" : "POPULATION_'"$A"'_1D_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"position" : "POSITION.mat"/"position" : "POSITION_'"$A"'_1D_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+
+        sed -i 's/"density" : "DENSITY.mat"/"density" : "DENSITY_'"$A"'_1D_E='"$I"'.mat"/' "input_1D_${A}_E=$I.json"
+    done
+done
+
+for I in $(seq 1.2 0.2 10); do
     for J in "${!MASS[@]}"; do
 
         M=${MASS[$J]}; A=${ATOM[$J]}; cp input_1D.json "input_1D_${A}_E=$I.json"
